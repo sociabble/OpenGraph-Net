30607c9c961c4d491a956fef6dc56a593a4adae9
coronag <guillaume@sociabble.com>
2017-12-20 17:36:35 +0100

    new unit test...

---
+++ src/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0bd9bf9b-ef04-4cc0-852d-e34d9b6da0ba")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ src/UnitTest/TwitterCardTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenGraph_Net;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class TwitterCardTest
+    {
+        protected OpenGraph ParseUrl(string u)
+        {
+            return OpenGraph.ParseUrl(u);
+        }
+
+        [TestMethod]
+        public void ParseUrl_TwitterCard_PlayerBandCamp()
+        {
+            var ogs = ParseUrl("https://rednoisefactory.bandcamp.com/album/ataraxia");
+            Assert.AreEqual(ogs.TwitterCards.Card, "player");
+            Assert.AreEqual(ogs.TwitterCards.Player, "https://bandcamp.com/EmbeddedPlayer/v=2/album=3274937510/size=large/linkcol=0084B4/notracklist=true/twittercard=true/");
+        }
+    }
+}
